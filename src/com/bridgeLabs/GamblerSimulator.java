2fcdfbcb213155d77a93ece99a8ccfac6d694fdc
mukulBillore <mukulbillore88@gmail.com>
2021-12-25 14:57:21 +0530

    Initial added : UC1 Stake and bet amounnt is added to simulator

--- src/com/bridgeLabs/GamblerSimulator.java
+++ src/com/bridgeLabs/GamblerSimulator.java
@@ -1,6 +1,14 @@
 package com.bridgeLabs;
 
 public class GamblerSimulator {
+	int Stake=100;
+	int bet=1;
+	
+	public GamblerSimulator(int stake, int bet) {
+		super();
+		Stake = stake;
+		this.bet = bet;
+	}
 
 	public static void main(String[] args) {
 		
