151a8ee7a3c62be9a55f37005783052345cb53b3
mukulBillore <mukulbillore88@gmail.com>
2021-12-25 15:32:37 +0530

    Added : UC3 added the resigning condition for gambler

--- src/com/bridgeLabs/GamblerSimulator.java
+++ src/com/bridgeLabs/GamblerSimulator.java
@@ -1,11 +1,15 @@
 package com.bridgeLabs;
+
 import java.util.Random;
+
 public class GamblerSimulator {
-	int everyDayStake=100;
-	int bet=1;
-	
-	//empty constructor for Default Value
-	public GamblerSimulator() {}
+	int everyDayStake = 100;
+	int bet = 1;
+
+	// empty constructor for Default Value
+	public GamblerSimulator() {
+	}
+
 	public GamblerSimulator(int stake, int bet) {
 		super();
 		everyDayStake = stake;
@@ -15,15 +19,18 @@ public class GamblerSimulator {
 	public static void main(String[] args) {
 		GamblerSimulator gambler1 = new GamblerSimulator();
 		Random rand = new Random();
-		int winOrLoose = rand.nextInt(2);
-		//win
-		if(winOrLoose==0) {
-			gambler1.everyDayStake=gambler1.everyDayStake+gambler1.bet;
-			
-		}
-		else {
-			gambler1.everyDayStake=gambler1.everyDayStake-gambler1.bet;
-		}
+		
+		while (gambler1.everyDayStake < 150 && gambler1.everyDayStake>50) {
+			int winOrLoose = rand.nextInt(2);
+			// win
+			if (winOrLoose == 0) {
+				gambler1.everyDayStake = gambler1.everyDayStake + gambler1.bet;
+
+			} // loose
+			else {
+				gambler1.everyDayStake = gambler1.everyDayStake - gambler1.bet;
+			}
+		}System.out.println("The totel stake after game of every day stake is "+gambler1.everyDayStake );
 	}
 
 }
